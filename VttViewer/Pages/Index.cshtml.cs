de29dec79f62739bc204b2c44d9fe50f9960b04a
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2018-12-14 20:56:46 -0500

    removing some stuff

--- VttViewer/Pages/Index.cshtml.cs
+++ VttViewer/Pages/Index.cshtml.cs
@@ -4,11 +4,12 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.IO;
+using System.Text;
 
 namespace VttViewer.Pages {
     public class IndexModel : PageModel {
         public void OnGet() {
-
         }
     }
 }
